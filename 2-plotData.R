b793981934e307d02583e41c2eb34d021500c8c7
Jonathan Peelle <peelle@gmail.com>
2020-06-11 14:26:05 -0500

    Improvements to plotting

--- 2-plotData.R
+++ 2-plotData.R
@@ -3,6 +3,7 @@
 
 library(ggplot2)
 library(ggpubr)
+library(dplyr)
 
 dataFileName <- "start700-2200_length300-1800_2020-04-29-2156.csv"
 
@@ -10,13 +11,26 @@ dataFileName <- "start700-2200_length300-1800_2020-04-29-2156.csv"
 
 dfAll <- read.csv(dataFileName, header=TRUE, sep=",")
 
-# replace ot1, ot2, ot3 with more friendly linear, quadratic, cubic
-# TODO
+dfAll$term <- as.factor(dfAll$term)
+
+dfAll$term <- recode(dfAll$term,
+                     ot1 = "linear",
+                     ot2 = "quadratic",
+                     ot3 = "cubic",
+                     Group1 = "age",
+                     SNR1 = "noise",
+                     Condition1 = "frequency",
+                     "ot1:Group1" = "linear x age",
+                     "ot1:SNR1" = "linear x noise",
+                     "ot1:Condition1" = "linear x frequency",
+                     "Group1:Condition1" = "age x frequency",
+                     "Group1:SNR1" = "age x noise",
+                     "Group1:SNR1:Condition1" = "age x noise x frequency")
 
 
-#---- plot ----
+#---- plot main effects of time ----
 
-termsToPlot <- c("ot1", "ot2", "ot3")
+termsToPlot <- c("linear", "quadratic", "cubic")
 
 # filter rows
 
@@ -43,10 +57,12 @@ plotpvalues = function (data, thisTerm) {
     theme_bw() +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     coord_equal() + 
-    scale_fill_gradient(low = "white", high = "gray") +
+    scale_fill_gradient(low = "white", high = "gray", limits = c(0, 1)) +
     xlab("Start Time (ms)") +
     ylab("Window Length (ms)") +
-    ggtitle(thisTerm)
+    ggtitle(thisTerm) +
+    theme(legend.position="none"
+    )
 }
 
 plotestimate = function (data, thisTerm) {
@@ -57,10 +73,12 @@ plotestimate = function (data, thisTerm) {
     theme_bw() +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
     coord_equal() + 
-    scale_fill_gradient2(low = "muted(blue)", mid = "white", high = "red", midpoint = 0) +
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
     xlab("Start Time (ms)") +
     ylab("Window Length (ms)") +
-    ggtitle(thisTerm)
+    ggtitle(thisTerm) +
+    theme(legend.position="none") +
+    NULL
 }
 
 plotsP <- lapply(termsToPlot, plotpvalues, data = dfAll)
@@ -68,3 +86,155 @@ plotsEstimate <- lapply(termsToPlot, plotestimate, data = dfAll)
 
 plotsAll <- c(plotsEstimate, plotsP)
 ggarrange(plotlist = plotsAll, nrow = 2, ncol = 3)
+
+
+
+
+
+
+#---- plot main effects of age, noise, and frequency ----
+
+termsToPlot <- c("age", "noise", "frequency")
+
+# filter rows
+
+par(mfrow=c(2, length(termsToPlot)))
+
+plist <- c()
+
+plotpvalues = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = p.value)) +
+    geom_raster() +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient(low = "white", high = "gray") +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none") +
+    NULL
+}
+
+plotestimate = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = estimate)) +
+    geom_raster() + 
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none") +
+    NULL
+}
+
+plotsP <- lapply(termsToPlot, plotpvalues, data = dfAll)
+plotsEstimate <- lapply(termsToPlot, plotestimate, data = dfAll)
+
+plotsAll <- c(plotsEstimate, plotsP)
+ggarrange(plotlist = plotsAll, nrow = 2, ncol = 3)
+
+
+
+
+#---- plot some interactions ----
+
+termsToPlot <- c("linear x age", "linear x noise", "linear x frequency")
+
+# filter rows
+
+par(mfrow=c(2, length(termsToPlot)))
+
+plist <- c()
+
+plotpvalues = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = p.value)) +
+    geom_raster() +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient(low = "white", high = "gray") +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none")
+}
+
+plotestimate = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = estimate)) +
+    geom_raster() + 
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none")
+}
+
+plotsP <- lapply(termsToPlot, plotpvalues, data = dfAll)
+plotsEstimate <- lapply(termsToPlot, plotestimate, data = dfAll)
+
+plotsAll <- c(plotsEstimate, plotsP)
+ggarrange(plotlist = plotsAll, nrow = 2, ncol = 3)
+
+
+
+#---- plot more interactions ----
+
+termsToPlot <- c("age x noise", "age x frequency", "age x noise x frequency")
+
+# filter rows
+
+par(mfrow=c(2, length(termsToPlot)))
+
+plist <- c()
+
+plotpvalues = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = p.value)) +
+    geom_raster() +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient(low = "white", high = "gray") +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none")
+}
+
+plotestimate = function (data, thisTerm) {
+  dfTmp <- data[data$term==thisTerm,]
+  
+  ggplot(dfTmp, aes(startTime, windowLength, fill = estimate)) +
+    geom_raster() + 
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    coord_equal() + 
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+    xlab("Start Time (ms)") +
+    ylab("Window Length (ms)") +
+    ggtitle(thisTerm) +
+    theme(legend.position="none")
+}
+
+plotsP <- lapply(termsToPlot, plotpvalues, data = dfAll)
+plotsEstimate <- lapply(termsToPlot, plotestimate, data = dfAll)
+
+plotsAll <- c(plotsEstimate, plotsP)
+ggarrange(plotlist = plotsAll, nrow = 2, ncol = 3)
+
+
