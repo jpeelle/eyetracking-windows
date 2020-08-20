03816802eac89ed344701e68ee59f4406544c72d
Jonathan Peelle <peelle@gmail.com>
2020-08-20 12:48:03 -0500

    [ENH] Added parameter estimates to p values

--- 3-plotSomeTimes.R
+++ 3-plotSomeTimes.R
@@ -3,9 +3,8 @@
 #---- Import libraries and set options ----
 
 library(ggplot2)
-library(lme4)
-library(grid)
-
+library(ggpubr)
+library(dplyr)
 
 startTime <- 1300
 windowLengths <- seq(from = 800, to = 1200, by = 100/2)
@@ -39,30 +38,32 @@ dfAll$term <- recode(dfAll$term,
                      "Group1:SNR1:Condition1" = "age:noise:frequency")
 
 
-# First filter for start time; then for durations
+# First filter for start time; then for durations; then for condition
+cond <- c("age", "noise", "frequency", "linear:age", "linear:noise", "linear:frequency")
 
 df <- dfAll[dfAll$startTime==1300,]
 df <- df %>% filter(windowLength %in% windowLengths)
-#df <- df[df$windowLength>windowLengthMin,]
-#df <- df[df$windowLength<windowLengthMax,]
-
-
-cond <- c("linear", "quadratic", "cubic", "age", "noise", "frequency", "linear:age", "linear:noise", "linear:frequency")
-
 df <- df %>% filter(term %in% cond)
 
-#---- plot p values grouped by effect, colored by windowLength ----
+#---- plot parameter estimates and p values grouped by effect, colored by windowLength ----
 
 df$windowLength <- as.factor(df$windowLength)
-ggbarplot(df, x = "term", y = "p.value", order = cond,
-          color = "windowLength", fill = "windowLength", 
-          palette = "RdBu",
-          position = position_dodge2(padding = 0.2)) +
+
+p1 <- ggbarplot(df, x = "term", y = "estimate", order = cond,
+                color = "windowLength", fill = "windowLength", 
+                palette = "RdBu",
+                position = position_dodge2(padding = 0.2)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ylab("parameter estimate")
+
+p2 <- ggbarplot(df, x = "term", y = "p.value", order = cond,
+                color = "windowLength", fill = "windowLength", 
+                palette = "RdBu",
+                position = position_dodge2(padding = 0.2)) +
   geom_hline(yintercept = .05, linetype = "dotted", color = "blue", size = 0.5) +
   annotate(geom="text", x = 1, y = .075, label="p = .05", color = "blue") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ylab("p value")
 
-
-
+ggarrange(p1, p2, ncol = 1, nrow = 2)
 
